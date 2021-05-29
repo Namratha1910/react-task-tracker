053f2f596d63f659a1db7b699ddf69cbcb940ef0
Namratha Siddula <amulyasiddula19@gmail.com>
2021-05-29 14:16:24 +0530

    Added Submit Task Button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+import AddTask from './components/AddTask'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30 pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Shopping',
+        day: 'Feb 6th at 2:30 pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Project Seminar',
+        day: 'Feb 19th at 2:30 pm',
+        reminder: false,
+    }
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id!==id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask />
+      {tasks.length >0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show')}
     </div>
   );
 }
